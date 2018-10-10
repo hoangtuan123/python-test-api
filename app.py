da31026941510c2b83c94edd47c43cfbc879b35f
hoangtuan123 <nghoangtuan92@gmail.com>
2018-10-10 19:04:33 +0700

    Update app.py
    update ui

--- app.py
+++ app.py
@@ -15,8 +15,8 @@ def hello():
     except RedisError:
         visits = "<i>cannot connect to Redis, counter disabled</i>"
 
-    html = "<h3>Hello {name}!</h3>" \
-           "<b>Hostname:</b> {hostname}<br/>" \
+    html = "<h3>Hello user {name}!</h3>" \
+           "<b>Hostname is:</b> {hostname}<br/>" \
            "<b>Visits:</b> {visits}"
     return html.format(name=os.getenv("NAME", "world"), hostname=socket.gethostname(), visits=visits)
 
